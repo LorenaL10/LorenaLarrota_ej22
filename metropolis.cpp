e877037ee212314cf7876feec5ddb514dc39d6c2
LorenaL10 <46940824+LorenaL10@users.noreply.github.com>
2019-04-04 12:12:10 -0500

    Update metropolis.cpp

--- metropolis.cpp
+++ metropolis.cpp
@@ -12,13 +12,13 @@ int main(){
     int f=exp(-x*x/2);
     srand48(5);
     
+    
 void metropolis{
 for(i=0;i<N;i++){ 
-        float g(float 
+    
         float propuesta  = drand48() + drand48()
-        float r = min(1,f(propuesta)/f(lista[i-1]))
-        alpha = np.random.random()
-        if(alpha<r):
+        float alpha = drand48()
+        if(alpha<propuesta):
             lista.append(propuesta)
         else:
             lista.append(lista[i-1])
