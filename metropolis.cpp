0a8f446d1d6b357282c09452a0670f7d62b4297d
LorenaL10 <46940824+LorenaL10@users.noreply.github.com>
2019-04-04 07:49:09 -0500

    Create metropolis.cpp

---
+++ metropolis.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using std::cout;
+using std::endl;
+
+void metropolis();
+
+int main(){
+    int N=1000;
+    int f=exp(-x*x/2);
+    srand48(5);
+    
+void metropolis{
+for(i=0;i<N;i++){ 
+        float g(float 
+        float propuesta  = drand48() + drand48()
+        float r = min(1,f(propuesta)/f(lista[i-1]))
+        alpha = np.random.random()
+        if(alpha<r):
+            lista.append(propuesta)
+        else:
+            lista.append(lista[i-1])
+    return np.array(lista)
+    }
+  }
